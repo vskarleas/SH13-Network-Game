d9053000990b6d142ade7c5a87381e18bd2eda75
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2025-01-23 18:23:38 +0100

    V5.3.2

--- cmd_4players.sh
+++ cmd_4players.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-gcc -o sh13 -I/us/include/SDL2 sh13_4.c -lSDL2_image -LSDL2_ttf -LSDL2 - Ipthread
+gcc -o sh13 -I/usr/include/SDL2 sh13_4.c -lSDL2_image -lSDL2_ttf -lSDL2 -lpthread
 gcc -o server server_4.c
